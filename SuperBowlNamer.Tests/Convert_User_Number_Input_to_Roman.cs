3fb0150ca8d0c4dfd51fb360a478abde5c1a0ffc
MCrank <marco_mcse@hotmail.com>
2019-04-02 20:10:20 -0500

    Stub out code to begin building tests

---
+++ SuperBowlNamer.Tests/Convert_User_Number_Input_to_Roman.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+
+namespace SuperBowlNamer.Tests
+{
+    public class Convert_User_Number_Input_to_Roman
+    {
+        [Fact]
+        public void Convert_User_Input_1_to_Roman_I()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
+
+//1	    I
+//5	    V
+//9	    IX
+//12	XII
+//16	XVI
+//29	XXIX
+//44	XLIV
+//45	XLV
+//68	LXVIII
+//83	LXXXIII
+//97	XCVII
+//99	XCIX
+//500	D
+//501	DI
+//649	DCXLIX
+//798	DCCXCVIII
+//891	DCCCXCI
+//1000	M
+//1004	MIV
+//1006	MVI
+//1023	MXXIII
+//2014	MMXIV
+//3999	MMMCMXCIX
---
+++ SuperBowlNamer/IntToRomanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBowlNamer
+{
+    /*
+     * 1 = I
+     * 5 = V
+     * 10 = X
+     * 50 = L
+     * 100 = C
+     * 500 = D
+     * 1000 = M
+   */
+
+    public class IntToRomanConverter
+    {
+        public string ConvertToRomanNumerals()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
