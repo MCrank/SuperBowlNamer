2e28b37cb2b0858194f21d58aaee2e9d67c27c75
MCrank <marco_mcse@hotmail.com>
2019-04-03 14:40:58 -0500

    Test passing for "9" to "IX"

--- SuperBowlNamer.Tests/Convert_User_Number_Input_to_Roman.cs
+++ SuperBowlNamer.Tests/Convert_User_Number_Input_to_Roman.cs
@@ -3,6 +3,32 @@ using Xunit;
 
 namespace SuperBowlNamer.Tests
 {
+    /* Numbers to test conversion
+     * 1    I
+     * 5	V
+     * 9	IX
+     * 12	XII
+     * 16	XVI
+     * 29	XXIX
+     * 44	XLIV
+     * 45	XLV
+     * 68	LXVIII
+     * 83	LXXXIII
+     * 97	XCVII
+     * 99	XCIX
+     * 500	D
+     * 501	DI
+     * 649	DCXLIX
+     * 798	DCCXCVIII
+     * 891	DCCCXCI
+     * 1000	M
+     * 1004	MIV
+     * 1006	MVI
+     * 1023	MXXIII
+     * 2014	MMXIV
+     * 3999	MMMCMXCIX
+     */
+
     public class Convert_User_Number_Input_to_Roman
     {
         [Fact]
@@ -34,30 +60,19 @@ namespace SuperBowlNamer.Tests
             Assert.Equal(expectedOutput, actualResult);
             
         }
-        
+
+        [Fact]
+        public void Convert_User_Input_9_to_Roman_IX()
+        {
+            // Arrange
+            var converter = new IntToRomanConverter();
+            var input = "9";
+            var expectedOutput = "IX";
+            // Act
+            var actualResult = converter.ConvertToRomanNumerals(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualResult);
+        }
     }
 }
-
-//1	    I
-//5	    V
-//9	    IX
-//12	XII
-//16	XVI
-//29	XXIX
-//44	XLIV
-//45	XLV
-//68	LXVIII
-//83	LXXXIII
-//97	XCVII
-//99	XCIX
-//500	D
-//501	DI
-//649	DCXLIX
-//798	DCCXCVIII
-//891	DCCCXCI
-//1000	M
-//1004	MIV
-//1006	MVI
-//1023	MXXIII
-//2014	MMXIV
-//3999	MMMCMXCIX
