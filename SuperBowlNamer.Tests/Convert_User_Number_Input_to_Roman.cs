0a8399ee5df0788db3e788a23f9abeef50d5a83a
MCrank <marco_mcse@hotmail.com>
2019-04-03 18:20:24 -0500

    Tests Pass "97" - "XCVII"

--- SuperBowlNamer.Tests/Convert_User_Number_Input_to_Roman.cs
+++ SuperBowlNamer.Tests/Convert_User_Number_Input_to_Roman.cs
@@ -173,5 +173,19 @@ namespace SuperBowlNamer.Tests
             Assert.Equal(expectedOutput, actualResult);
         }
 
+        [Fact]
+        public void Convert_User_Input_97_to_Roman_XCVII()
+        {
+            // Arrange
+            var converter = new IntToRomanConverter();
+            var input = "97";
+            var expectedOutput = "XCVII";
+            // Act
+            var actualResult = converter.ConvertToRomanNumerals(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualResult);
+        }
+
     }
 }
